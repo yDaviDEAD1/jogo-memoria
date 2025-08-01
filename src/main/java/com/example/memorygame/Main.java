5fd40b6e13ebeed612c3065d749c87d8688bceed
Davi Meireles <ydavidead@gmail.com>
2025-08-01 12:27:29 -0300

    fix image readMe

--- src/main/java/com/example/memorygame/Main.java
+++ src/main/java/com/example/memorygame/Main.java
@@ -10,10 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // CORREÇÃO: Carrega a tela de menu como ponto de entrada
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Memory Game - Menu"); // Título da janela do menu
+        stage.setTitle("Memory Game - Menu");
         stage.setScene(scene);
         stage.show();
     }
